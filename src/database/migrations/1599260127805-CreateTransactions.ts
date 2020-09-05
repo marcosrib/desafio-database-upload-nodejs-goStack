efd702792c22831e4a190326d73132cab1d24ff7
marcos <marcos.rib.sousa@gmail.com>
2020-09-04 22:33:11 -0300

    createed trasactions

---
+++ src/database/migrations/1599260127805-CreateTransactions.ts
@@ -0,0 +1,50 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateTransactions1599260127805
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'transactions',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'value',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(_queryRunner: QueryRunner): Promise<void> {
+    await _queryRunner.dropTable('transactions');
+  }
+}
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,55 @@
+import { getRepository, TransactionRepository } from 'typeorm';
+
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
 
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getRepository(Transaction);
+    const categoryRepository = getRepository(Category);
+    const categoryResult = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    let category_id = '';
+
+    if (!categoryResult) {
+      const categoryObject = categoryRepository.create({
+        title: category,
+      });
+      await categoryRepository.save(categoryObject);
+      category_id = categoryObject.id;
+    }
+
+    if (categoryResult) {
+      category_id = categoryResult.id;
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
